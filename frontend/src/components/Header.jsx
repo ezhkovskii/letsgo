feef27c2f456d8a215f1ed90ff82b948d25ea83d
Arthur <you@example.com>
2021-08-21 02:48:39 +0500

    front edit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,17 +3,25 @@ import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
-        <ul>
-            <li>
-                <Link to="/">Пресс-туры</Link>
-            </li>
-            <li>
-                <Link to="/feedback">Обратная связь</Link>
-            </li>
-            <li>
-                <Link to="/monitoring">Мониторинг</Link>
-            </li>
-        </ul>  
+        <div className={styles.header + " ws-flexbox"}>
+            <div className={styles.leftBlock}>
+                <Link className={styles.link} to="/">Пресс-туры</Link>
+            </div>
+            <div className={styles.rightBlock}>
+                <Link className={styles.link} to="/">Пресс-туры</Link>
+            </div>
+        </div>
+        // <ul>
+        //     <li>
+        //         <Link to="/">Пресс-туры</Link>
+        //     </li>
+        //     <li>
+        //         <Link to="/feedback">Обратная связь</Link>
+        //     </li>
+        //     <li>
+        //         <Link to="/monitoring">Мониторинг</Link>
+        //     </li>
+        // </ul>  
     );
 }
   
