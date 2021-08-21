63f84200713d28150d5c46f55dfff431dc751f64
Alexander <ezhkovskii@gmail.com>
2021-08-21 06:10:38 +0500

    add main models

--- letsgo/letsgoapi/admin.py
+++ letsgo/letsgoapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from letsgoapi import models
 
-# Register your models here.
+admin.site.register(models.PressTour)
+admin.site.register(models.Blogger)
+admin.site.register(models.AccountInsta)
+admin.site.register(models.ParamSearch)
+admin.site.register(models.MemberPressTour)
--- letsgo/letsgoapi/models.py
+++ letsgo/letsgoapi/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class AccountInsta(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    session_id = models.CharField(max_length=255)
+    instagram_id = models.BigIntegerField()
+
+    def __str__(self):
+        return self.username
+
+
+class PressTour(models.Model):
+
+    START = 1
+    ONGOING = 2
+    END = 3
+    STATUS = [
+        (START, 'Набор блогеров'),
+        (ONGOING, 'Проведение'),
+        (END, 'Завершен'),
+    ]
+
+    title = models.CharField(max_length=255)
+    status = models.IntegerField(choices=STATUS, default=START)
+    number_bloggers = models.IntegerField()
+    created = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class ParamSearch(models.Model):
+
+    MALE = 1
+    FEMALE = 2
+    SEX = [
+        (MALE, 'Мужчина'),
+        (FEMALE, 'Женщина'),
+    ]
+    
+    press_tour = models.OneToOneField(
+        PressTour,
+        on_delete=models.CASCADE
+    )
+    key_words = ArrayField(models.CharField(max_length=255), blank=True)
+    sex = models.IntegerField(choices=SEX, default=FEMALE)
+    involvement = models.FloatField(verbose_name='Вовлеченность')
+    number_publications = models.IntegerField(verbose_name='Количество публикаций')
+    number_subscribers = models.IntegerField(verbose_name='Количество подписчиков')
+    number_subscriptions = models.IntegerField(verbose_name='Количество подписок')
+
+
+    def __str__(self):
+        return 'Параметры поиска: ' + self.press_tour.title
+
+
+class Blogger(models.Model):
+    username = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
+    instagram_id = models.BigIntegerField()
+    press_tour = models.ManyToManyField(PressTour, through='MemberPressTour')
+
+
+class MemberPressTour(models.Model):
+
+    ACCEPTED = 1
+    PENDING = 2
+    DENIED = 3 
+    STATUS = [
+        (ACCEPTED, 'Согласен'),
+        (PENDING, 'В ожидании ответа'),
+        (DENIED, 'Отказался'),
+    ]
+
+    blogger = models.ForeignKey(Blogger, on_delete=models.CASCADE)
+    press_tour = models.ForeignKey(PressTour, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=PENDING)
+
+    def __str__(self):
+        return self.press_tour + ' ' + self.blogger
